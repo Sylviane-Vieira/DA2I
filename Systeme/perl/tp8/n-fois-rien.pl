dd5fc73877c2aeca5d0bde65359928a90fef66c3
Sylviane <sylviane.vieira@gmail.com>
2017-12-07 22:06:11 +0100

    perl tp8

--- Systeme/perl/tp8/n-fois-rien.pl
+++ Systeme/perl/tp8/n-fois-rien.pl
@@ -3,13 +3,19 @@
 use strict;
 use warnings;
 use Socket;
+use POSIX ":sys_wait_h";
 
 my $n = $ARGV[0];
 my $pere = $$;
 
-for (my $i = 1; $i<$n; $i++){
+for (my $i=0; $i<$n; $i++){
   my $fils = fork();
-  if ($fils != 0) {
-    print "je suis le pere de pid : $pere, mon fils est : $fils\n";
-  }
+  #if ($fils != 0) {
+    print "\ncréation du processus fils de pid $fils de père $$\n";
+    #`chmod +x ./rien.pl`;
+    #print `./rien.pl`;
+    my $recup = (waitpid($fils, &WNOHANG));
+    if ($recup == $fils)
+      { print "\nRecuperation du fils $fils\n";}
+  #}
 }
