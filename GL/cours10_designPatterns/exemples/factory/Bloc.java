daa1cfea66734ac815ee73da13c415fc84a6e19f
Sylviane <sylviane.vieira@gmail.com>
2017-12-16 17:20:07 +0100

    conf

---
+++ GL/cours10_designPatterns/exemples/factory/Bloc.java
@@ -0,0 +1,13 @@
+public abstract class Bloc {
+    protected double masse_volumique;
+
+    public Bloc(double mv) {
+	masse_volumique = mv;
+    }
+    
+    public double masse() {
+	return masse_volumique * volume();
+    }
+    
+    public abstract double volume(); // signature seule
+}
---
+++ GL/cours10_designPatterns/exemples/singleton/classes_anonymes/I.java
@@ -0,0 +1,3 @@
+public interface I {
+    int operation(int a);
+}
